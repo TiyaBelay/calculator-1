efc759fdba8598d0b803e733792cba2732bf5a22
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:39:46 -0700

    Addition and Subtraction

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+def add(num1, num2):
+    """ add the arguments"""
+    total = num1 + num2 
+    return total
+
+def subtract(num1, num2):
+    return num1 - num2 
+
+def multiply(num1, num2):
+    pass
+
+def divide(num1, num2):
+    pass
+
+def square(num1):
+    pass
+
+def cube(num1):
+    pass
+
+def power(num1, num2):
+    pass
+
+def mod(num1, num2):
+    pass
